b93b3a65ac558b0676cdeb3f7a2c283c4e0a5499
hobr86 <hobr86@gmail.com>
2020-08-19 18:58:42 -0500

    evenodd

---
+++ Chap06/even_odd_array.py
@@ -0,0 +1,45 @@
+import collections
+import functools
+from typing import List
+
+from test_framework import generic_test
+from test_framework.test_failure import TestFailure
+from test_framework.test_utils import enable_executor_hook
+
+
+def even_odd(A: List[int]) -> None:
+    # TODO - you fill in here.
+    # 1 2 3 4 5 6 7 
+    l = 0
+    r = len(A) - 1
+
+    while l < r:
+        if A[l] & 1 == 0:
+            l += 1
+        else:
+            A[l], A[r] = A[r], A[l]
+            r -= 1
+
+
+@enable_executor_hook
+def even_odd_wrapper(executor, A):
+    before = collections.Counter(A)
+
+    executor.run(functools.partial(even_odd, A))
+
+    in_odd = False
+    for a in A:
+        if a % 2 == 0:
+            if in_odd:
+                raise TestFailure('Even elements appear in odd part')
+        else:
+            in_odd = True
+    after = collections.Counter(A)
+    if before != after:
+        raise TestFailure('Elements mismatch')
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('even_odd_array.py',
+                                       'even_odd_array.tsv', even_odd_wrapper))
